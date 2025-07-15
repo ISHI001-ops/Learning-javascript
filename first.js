50629b76229c9c0c3bda203433f9211497df57bb
ISHI JAIN <ishi.jain.official@gmail.com>
2025-07-15 18:07:09 +0530

    initialize repo

---
+++ first.js
@@ -0,0 +1,51 @@
+// Browser <-- Html <--- JS 
+//css -> link tag
+//JS -> script tag 
+// = ko hum assignment operator kehte hai = right side ki value ko left side ki value mai jaa ke save ker raha hai
+// curly brases jaha se shuru hote hai vaha se naya code shuru hota hai
+// apan ko pure collection ko change karna hoga ek object ko change karne ke liye
+// let.. ko hum update ker sakte hai, const ko nahi, but const obj-->key ko ker sakte hai
+
+const profile = {
+   fullName:"ishi jain",
+   followers:569,
+   following:4,
+
+
+}; 
+console.log(profile)
+
+// console.log("hljhjf")
+
+//Arimetic Operators
+
+let a = 5;
+let b = 2;
+console.log("a =",a,"& b =",b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b); 
+// inrement operator means ++ or decrement means --
+
+
+//Unary Operator
+let c = 4;
+let d = 8;
+
+console.log("c =",c,"& d =",d)
+c = c-1;
+console.log("c=",c);
+//a++(post) , ++a(pre)
+//a--(post) , --a(pre)
+
+
+
+
+
+
+
+
+
+
