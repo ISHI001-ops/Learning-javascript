956a8353db50651bcc69158c748c886fa497ae5b
ISHI JAIN <ishi.jain.official@gmail.com>
2025-08-23 21:51:21 +0530

    practice question 2

--- first.js
+++ first.js
@@ -153,6 +153,7 @@ console.log(result);
 
 
 
+// practice question 1
 let num = prompt("enter a number");
 
 if(num % 5 === 0) {
@@ -161,6 +162,25 @@ if(num % 5 === 0) {
    console.log(num,"is NOT a multiple of 5");
 }
   
+// practice question 2
+
+let score = 75;
+let grade;
+
+if(score >= 90 && score <= 100) {
+   grade = "A";
+} else if(score >= 70 && score <= 89) {
+   grade = "B";
+} else if(score >= 60 && score <= 69) {
+   grade = "C";
+} else if(score >= 50 && score <= 59) {
+   grade = "D";
+} else if(score >= 0 && score <= 49) {
+   grade = "F"
+}
+
+console.log("according to your scores, your grade was : ", grade)
+
 
 
 
