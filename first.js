f672d2c75853bfa3e5ffb100f4109deb9e08624a
ISHI JAIN <ishi.jain.official@gmail.com>
2025-12-14 21:31:03 +0530

    Updated first.js

--- first.js
+++ first.js
@@ -669,3 +669,15 @@ console.log(headings);
 
 // let allEL = document.querySelectorAll("p"); //all elements
 // console.dir(allEL);
+
+let div = document.querySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+let name = div.getAttribute("name");
+console.log(name);
+
+let para = document.querySelector("p");
+console.log(para.getAttribute("class"));
